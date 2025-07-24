26132968aae289efececb9f119a1898b2ed7fbbd
Ryan Obear <obearyan@umich.edu>
2025-07-24 13:20:51 -0400

    remove filter for stores

--- app/create_stores.py
+++ app/create_stores.py
@@ -6,5 +6,5 @@ from tqdm import tqdm
 """Run this code to populate vectore stores. THis should only be run ONCE"""
 if __name__ == "__main__":
     #pass
-    for appid in tqdm(pd.read_csv('../data/game_player_cnt_ranked_top_1k.csv')['appid'].values[466:]):
+    for appid in tqdm(pd.read_csv('../data/game_player_cnt_ranked_top_1k.csv')['appid'].values):
         get_review_retriever(get_reviews(appid), skip_populating=False, filter_app_id=appid)
