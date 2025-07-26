1b82e2f66ad3ac49854979de7f20eedc3dd6936f
Ryan Obear <obearyan@umich.edu>
2025-07-25 21:07:00 -0400

    adding game data chroma stores

--- app/create_stores.py
+++ app/create_stores.py
@@ -1,10 +1,11 @@
-from llm import get_reviews, get_review_retriever
+from llm import get_reviews, get_review_retriever, get_game_data_retriever
 import pandas as pd
 from tqdm import tqdm
 
 
 """Run this code to populate vectore stores. THis should only be run ONCE"""
 if __name__ == "__main__":
-    #pass
+    pass
+    get_game_data_retriever()
     for appid in tqdm(pd.read_csv('../data/game_player_cnt_ranked_top_1k.csv')['appid'].values):
         get_review_retriever(get_reviews(appid), skip_populating=False, filter_app_id=appid)
